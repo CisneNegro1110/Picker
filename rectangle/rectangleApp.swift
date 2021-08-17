92b5350bb1fd3372b3b92028dcf136ad8e62fd34
JesúsFranciscoLeyva <leyva527@gmail.com>
2021-08-17 16:35:10 -0600

    Initial Commit

---
+++ rectangle/rectangleApp.swift
@@ -0,0 +1,17 @@
+//
+//  rectangleApp.swift
+//  rectangle
+//
+//  Created by Jesús Francisco Leyva Juárez on 17/08/21.
+//
+
+import SwiftUI
+
+@main
+struct rectangleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
